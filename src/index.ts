ce75c290434f9001300b8f343240cf93bec7fab5
ramki venkatachalam <ramkiv@zenefits.com>
2021-06-04 12:08:33 -0700

    add error handling

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import cors from "cors";
 import helmet from "helmet";
 
 import { itemsRouter } from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
+
 
 dotenv.config();
 
@@ -30,6 +33,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api/menu/items", itemsRouter);
+app.use(errorHandler);
+//app.use(notFoundHandler);
 /**
  * Server Activation
  */
